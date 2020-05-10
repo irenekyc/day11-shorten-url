9f74b694e689bbe5a17be8670d178331592e7a53
irenekyc <gugu_irene@hotmail.com>
2020-05-10 11:27:46 +0800

    desktop static version finished, also add nav bar background color changed on scroll

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,11 @@ menuBtn.addEventListener('click', ()=>{
     menuBtn.classList.toggle('show')
     navMenu.classList.toggle('show')
 })
+
+window.addEventListener('scroll', e=>{
+    if(window.scrollY> 60){
+       document.querySelector('nav').classList.add('white-bg')
+    } else {
+        document.querySelector('nav').classList.remove('white-bg')
+    }
+})
